b4e663dc4ab425d737017b92bfa9ee32c8d82e98
NAVGURUKUL <ashurwt455@gmail.com>
2024-09-18 13:43:56 +0530

    Initial commit

---
+++ interview_management/interview/doctype/interviewer_schedule/interviewer_schedule.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, asha rawat and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("interviewer schedule", {
+// 	refresh(frm) {
+
+// 	},
+// });
